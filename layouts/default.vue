9af4a57e8a855641020e6a396f40f275274c0383
FIAP <FIAP>
2019-12-12 22:19:45 -0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <!-- navigation -->
+    <nav class="navbar is-white" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-start">
+          <nuxt-link to="/" class="navbar-item">Awesome JS SSR Blog!</nuxt-link>
+          <nuxt-link active-class="is-active" to="/" class="navbar-item is-tab" exact>Home</nuxt-link>
+          <nuxt-link active-class="is-active" to="/about" class="navbar-item is-tab" exact>About</nuxt-link>
+        </div>
+      </div>
+    </nav>
+    <!-- /navigation -->
+    <nuxt />
+  </div>
+</template>
+
+
+<style>
+.main-content {
+  margin: 30px 0;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
